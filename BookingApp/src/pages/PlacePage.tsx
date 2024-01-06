2d78566b1f40bae939e4c1c3d4f43acb51f44b69
Chakradhar Dwivedi <chakradhar.dwivedi2000@gmail.com>
2024-01-06 15:53:59 +0530

    PlacePage added

--- BookingApp/src/pages/PlacePage.tsx
+++ BookingApp/src/pages/PlacePage.tsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { Link, useParams } from "react-router-dom";
+import {  useParams } from "react-router-dom";
 import { Place } from "../models/Place";
 
 export default function PlacesPage() {
